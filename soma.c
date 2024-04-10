b459f69b2382fa85329d9d56139da9a75e61fe34
Pedro Henrique da Silva Santos <160782034+PedroHSS164@users.noreply.github.com>
2024-04-10 16:02:25 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,29 @@
+void main (){
+
+int n1, n2, n3, n4, soma;
+
+printf("\ndigite um numero: ");
+scanf("%d",&n1);
+printf("\ndigite um numero: ");
+scanf("%d",&n2);
+printf("\ndigite um numero: ");
+scanf("%d",&n3);
+printf("\ndigite um numero: ");
+scanf("%d",&n4);
+
+soma = (n1+n2+n3+n4);
+
+printf("resultado da soma: %d",soma);
+
+
+
+
+
+
+
+
+
+
+
+
+}
